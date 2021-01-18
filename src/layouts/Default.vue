4c9a687c291aed003ddda7b33c4a78f43a6b0179
Benjamin Borowski <ben@weareokidoki.com>
2021-01-17 23:36:50 -0800

    dev: make nav rendered list

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -5,10 +5,9 @@
 
       <nav>
         <ul class="flex space-x-3 uppercase">
-          <li><g-link to="/">Home</g-link></li>
-          <li><g-link to="/devlog/">Devlog</g-link></li>
-          <li><g-link to="/now/">Now</g-link></li>
-          <li><g-link to="/gear/">Gear</g-link></li>
+          <li v-for="item in this.nav" :key="item.path">
+            <g-link :to="item.path">{{ item.title }}</g-link>
+          </li>
         </ul>
       </nav>
     </header>
@@ -24,3 +23,18 @@
     }
   }
 </static-query>
+
+<script>
+  export default {
+    computed: {
+      nav() {
+        return [
+          { title: 'Home', path: '/' },
+          { title: 'Devlog', path: '/devlog/' },
+          { title: 'Now', path: '/now/' },
+          { title: 'Gear', path: '/gear/' },
+        ];
+      },
+    },
+  };
+</script>
