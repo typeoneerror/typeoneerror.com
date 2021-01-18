76f9287477a56943dd7b97b08022aa23e58fdefb
Benjamin Borowski <ben@weareokidoki.com>
2021-01-17 19:39:40 -0800

    dev: adds autoprefix and purge

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,8 +4,24 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const tailwind = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+const postcssPlugins = [tailwind()];
+
+if (process.env.NODE_ENV === 'production') {
+  postcssPlugins.push(purgecss(require('./purgecss.config.js')));
+}
+
 module.exports = {
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins,
+      },
+    },
+  },
+  plugins: [],
   siteName: 'TYPEONEERROR',
   siteUrl: 'https://typeoneerror.com',
-  plugins: [],
 };
